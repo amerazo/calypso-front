cf21106800ff6b9a733025a6c1290a55e0459e4f
Laura Ostering <laurabeepboop@Lauras-MacBook-Pro.local>
2023-05-13 13:15:33 -0700

    beginning frontend

---
+++ frontend/src/components/Task.js
@@ -0,0 +1,7 @@
+function Task(props) {
+    return (
+        <h1>Task Component</h1>
+    )
+};
+
+export default Task;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,30 @@
+// import bootstrap stuff
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Card, Container } from 'react-bootstrap'; 
+
+function Home(props) {
+    return (
+        <Container style={{ display: 'flex', flexDirection: 'row' }}>
+            <Card style={{ flex: 1, width: '20rem' }}>
+                <Card.Body>
+                    <Card.Title>About Calypso</Card.Title>
+                    <Card.Text>
+                        Thanks for visiting Calypso! We'd love to share more about our project and us. 
+                    </Card.Text>
+                    <Button href="/about" variant="primary">About</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ flex: 1, width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>Log In</Card.Title>
+                    <Card.Text>
+                        Please login to see your projects, or to create a new one. 
+                    </Card.Text>
+                    <Button href="/boards" variant="primary">Log In</Button>
+                </Card.Body>
+            </Card>
+        </Container>
+    )
+  };
+  
+  export default Home;
